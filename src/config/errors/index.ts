61e2061056d13e5705f6793cba252f2444474186
Mudassir Siddiqui <59256774+mudassir693@users.noreply.github.com>
2025-01-12 02:33:11 +0500

    Update index.ts

--- src/config/errors/index.ts
+++ src/config/errors/index.ts
@@ -1,5 +1,7 @@
 export const ApplicationErrors = {
     ACCESS_NOT_ALLOWED: "access not allowed",
     NOT_FOUND: "entity not found",
-    ACCOUNT_NOT_FOUND: "account not found"
+    ACCOUNT_NOT_FOUND: "account not found",
+    ACCOUNT_NOT_FOUND_2: "account not found",
+    
 }
